0bc29a3843be2ae680911f56b8f32391f9cc821e
Masaki Ozawa <ozamasa@shiolab.com>
2025-09-19 11:27:00 +0900

    change twitter client

--- utils/twitter.py
+++ utils/twitter.py
@@ -1,33 +1,29 @@
 import os
 import tweepy
 
+# 認証情報の取得（初期化時のみ）
+consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
+consumer_secret = os.getenv("TWITTER_CONSUMER_SECRET")
+access_token = os.getenv("TWITTER_ACCESS_TOKEN")
+access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+
+# グローバルな認証オブジェクト（v1.1 用）
+auth_v1 = tweepy.OAuth1UserHandler(
+    consumer_key, consumer_secret, access_token, access_token_secret
+)
+api_v1 = tweepy.API(auth_v1)
+
+# v2 クライアント（bearer_tokenは不要）
+client_v2 = tweepy.Client(
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret
+)
+
 def post_tweet(text, media_path=None):
-    consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
-    consumer_secret = os.getenv("TWITTER_CONSUMER_SECRET")
-    access_token = os.getenv("TWITTER_ACCESS_TOKEN")
-    access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
-    bearer_token = os.getenv("TWITTER_BEARER_TOKEN")
-
-    if not all([consumer_key, consumer_secret, access_token, access_token_secret, bearer_token]):
-        print("[Error] Twitter API credentials are missing.")
-        return False
-
-    # v1.1 用 (media upload)
-    auth_v1 = tweepy.OAuth1UserHandler(
-        consumer_key, consumer_secret, access_token, access_token_secret
-    )
-    api_v1 = tweepy.API(auth_v1)
-
-    # v2 用 (tweet post)
-    client_v2 = tweepy.Client(
-        bearer_token=bearer_token,
-        consumer_key=consumer_key,
-        consumer_secret=consumer_secret,
-        access_token=access_token,
-        access_token_secret=access_token_secret
-    )
-
     media_id = None
+
     if media_path:
         try:
             media = api_v1.media_upload(media_path)
